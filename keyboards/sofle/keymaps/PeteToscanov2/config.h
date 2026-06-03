e14b4e3671e6345734d612bd775da7f969acd257
Pete Toscano <pete@arin.net>
2026-06-02 22:14:10 -0400

    Forked from (v1) to try to add light support. No love so far.

---
+++ keyboards/sofle/keymaps/PeteToscanov2/config.h
@@ -0,0 +1,16 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 3
+#define TRI_LAYER_ADJUST_LAYER 4
+
+// https://docs.qmk.fm/features/caps_word
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_INVERT_ON_SHIFT
+
+// #define RGB_DI_PIN D3
+// #define RGBLED_NUM 72
+// #define RGBLED_SPLIT { 36, 36 }
